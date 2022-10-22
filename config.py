b3c99321574355d4a5b481614dc11884ffe19297
Eric Renart <eric.j.renart@gmail.com>
2022-10-22 17:38:03 -0400

    implemented basic config io

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
 from configparser import ConfigParser
+from typing_extensions import Self
 
 class ConfigManager:
 
     def __init__(self, config_path='config.cfg'):
-        self.read_configfile(config_path)
-
-    def read_configfile(self, config_path):
-        self.config = ConfigParser.read_file(config_path)
+        self.config = ConfigParser()
+        self.config.read_file(config_path)
     
-    def get_section(self, name):
-        if not self.config.has_section(name):
-            raise ValueError(f'Section {name} not found in configfile')
+    def get(self, section, value):
+        """
+        Gets the specified value from the specified section in the config file.
+        :param section: Section of value to retrieve
+        :param value: Value to retrieve
+        :return: config file value
+        """
+        if not self.config.has_section(section):
+            raise ValueError(f'Section {section} not found in config file')
+        if self.config[section][value] is None:
+            raise ValueError(f'Key {value} was not found in config file section {section}')
+        return self.config[section][value]
